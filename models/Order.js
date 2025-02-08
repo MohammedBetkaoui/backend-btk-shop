1f29f1f9b97839ad002fdf24c784b709fd277423
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2025-02-08 23:12:15 +0100

    get Orders for admin

--- models/Order.js
+++ models/Order.js
@@ -1,10 +1,9 @@
-// models/Order.js
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
   products: [{
-    productId: { type: Number, required: true }, // Changez ObjectId en Number
+    productId: { type: Number, required: true },
     quantity: { type: Number, required: true },
     size: { type: String, required: true },
   }],
